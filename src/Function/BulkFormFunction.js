6c6e93f01c46dfa5de6d8452e4c6ca1137842a2d
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-08-21 18:26:35 +0600

    adding bulk form

--- src/Function/BulkFormFunction.js
+++ src/Function/BulkFormFunction.js
@@ -108,35 +108,6 @@ export const handleChange = (index, e, formData, setFormData, fieldName) => {
     ]
 } 
 
-export const videoTemplateSubCategoryConstant = {
-    subCategory:[
-      {
-        subCategoryName:"Social media video templates",
-        subCategoryLink:"social-media-video-templates",
-      },
-      {
-        subCategoryName:"Youtube video templates",
-        subCategoryLink:"youbube-video-templates",
-      },
-      {
-        subCategoryName:"X/Twitter video templates",
-        subCategoryLink:"x-twitter-video-templates",
-      },
-      {
-        subCategoryName:"Facebook video templates",
-        subCategoryLink:"facebook-video-templates",
-      },
-      {
-        subCategoryName:"Instagram video templates",
-        subCategoryLink:"instagram-video-templates",
-      },
-      {
-        subCategoryName:"LinkedIn video templates",
-        subCategoryLink:"linkedin-video-templates",
-      }
-  
-    ]
-  }
 
 //   Application Supported 
 export const videoTemplateApplicationSupportedConstant = {
